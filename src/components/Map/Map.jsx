fcd53982acae4f4c19954ab95c44c1122e8f5f73
Samuel Ajala <samuelajala01@gmail.com>
2023-01-01 14:56:05 +0000

    setup GoogleMapReact

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,30 @@
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOutlinedicon from "@material-ui/icons/LocationOutlined";
+import Rating from "@material-ui/lab";
+
+import useStyles from "./styles";
+
 const Map = () => {
-  return <h1>Map</h1>;
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width: 600px)");
+
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
